cce10076a5877a2f2561f27fc936d893339771b1
JupDev <97832488+JupDiv@users.noreply.github.com>
2023-03-06 18:21:18 +0200

    delete part of import

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { useAppSelector } from '../../utils/hooks';
 import Product from '../Product/Product';
 import { BasketState, ProductProps } from '../../types/commonTypes';
